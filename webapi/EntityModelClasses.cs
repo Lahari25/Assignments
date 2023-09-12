3e17ea9df6b4f5ce0cdfd54dc67c362cc2c69c01
Lahari25 <79203383+Lahari25@users.noreply.github.com>
2023-09-12 15:08:20 +0530

    Add files via upload

---
+++ webapi/EntityModelClasses.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class Product
+    {
+       
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public int UnitPrice { get; set; }
+        public string Category { get; set; }
+    }
+
+        public class PProductDbContext : DbContext
+        {
+            public DbSet<Product> Products { get; set; }
+
+            public PProductDbContext(DbContextOptions<PProductDbContext> options)
+             : base(options)
+            {
+
+            }
+        }
+    }
+
